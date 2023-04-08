fa576b8e7231bd78348fd511aa925d47b785ff7c
raobaba <shrikishunr7@gmail.com>
2023-04-08 16:49:31 +0530

    delete header component

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
-import { AppBar, Toolbar, styled } from "@mui/material";
-import Header from './Header';
+import { AppBar, Toolbar, styled, Typography } from "@mui/material";
+import { Link } from 'react-router-dom';
 
 const StyledHeader = styled(AppBar)`
     background:#2874f2;
-    height: 55px;
+    height: 55px; 
 `
-
+const StyledLink = styled(Link)`
+    color: wheat;
+    text-decoration: none;
+    margin-right: 12%;
+ `
+ const Container = styled(Toolbar)`
+     display: flex;
+     justify-content: space-around;
+ `
 export default function Navbar() {
     return (
         <StyledHeader position="static">
-            <Toolbar>
-                <Header />
-            </Toolbar>
+            <Container>
+                <Typography>
+                    <StyledLink>TotalPost</StyledLink>
+                </Typography>
+                <Typography>
+                    <StyledLink>TotalUser</StyledLink>
+                </Typography>
+                <Typography>
+                    <StyledLink>AllPosts</StyledLink>
+                </Typography>
+                <Typography>
+                    <StyledLink>AllUsers</StyledLink>
+                </Typography>
+                <Typography>
+                    <StyledLink>PostForm</StyledLink>
+                </Typography>
+                <Typography>
+                    <StyledLink>UserForm</StyledLink>
+                </Typography>
+            </Container>
         </StyledHeader>
     )
 }
